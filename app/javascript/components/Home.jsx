5a8396dceb6a119a5da38c5922d654137c3c1959
Chi Tran <chidtran@gmail.com>
2020-03-11 16:04:59 -0400

    Integrate React as Rails FE

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => (
-  <div className='vw-100 vh-100 primary-color d-flex align-items-center justify-content-center'>
-    <div className='jumbotron jumbotron-fluid bg-transparent'>
-      <div className='container secondary-color'>
-        <h1 classname='display-4'>recipes</h1>
-        <p classname='lead'>
-          a curated list of recipes for the best homemade meals and delicacies
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Recipes</h1>
+        <p className="lead">
+          A curated list of recipes for the best homemade meals and delicacies
         </p>
-        <hr classname='my-4' />
+        <hr className="my-4" />
         <Link
-         to='/recipes'
-         classname='btn btn-lg custom-button'
-         role='button'
+          to="/recipes"
+          className="btn btn-lg custom-button"
+          role="button"
         >
-         view recipes
-       </Link>
+          View Recipes
+        </Link>
       </div>
     </div>
   </div>
--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom';
-import 'bootstrap/dis/css/bootstrap.min.css';
+import React from "react";
+import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'boostrap/dist/js/bootstrap.bundle.min';
-import App from '../components/App';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from "../components/App";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   render(
-    <App />
-    document.body.appendChild(document.createElement('div'))
+    <App />,
+    document.body.appendChild(document.createElement("div"))
   );
 });
