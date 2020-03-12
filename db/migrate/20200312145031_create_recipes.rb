22047bf11793a6f0053252a4dd36045b7a8042af
Chi Tran <chidtran@gmail.com>
2020-03-12 13:56:43 -0400

    Add seeds

--- db/migrate/20200312145031_create_recipes.rb
+++ db/migrate/20200312145031_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
     create_table :recipes do |t|
       t.string :name, null: false
       t.text :ingredients, null: false
-      t.text :insutrction, null:false
+      t.text :instruction, null:false
       t.string :image, default: 'https://images.food52.com/Ce-8AAd0X-DEr0uVqyLZotvCWNU=/1536x1022/2e8f6f4b-fa0e-40cb-abaf-14ae8b1ce39e--062210F_351.JPG'
 
       t.timestamps
